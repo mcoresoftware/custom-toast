fdd5271f1e8da1d15dc0be13fb44926cde70d9dc
mcoresoftware <32482124+mcoresoftware@users.noreply.github.com>
2017-11-02 13:42:33 +0100

    Added project files.

---
+++ app/src/main/java/com/mcoresoftware/custom_toast/MainActivity.java
@@ -0,0 +1,12 @@
+package com.mcoresoftware.custom_toast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
